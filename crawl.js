dc8b5276c19ce75edbe03850582d1f6d90a6b6fe
Sousa-heder <hedersonbalbino@gmail.com>
2023-08-22 18:04:57 -0300

    normalizeURL and tests

--- crawl.js
+++ crawl.js
@@ -1,6 +1,12 @@
 function normalizeURL(urlString){
-    return urlString
+    const ulrObj = new URL(urlString)
+    const hostPath = `${ulrObj.hostname}${ulrObj.pathname}`
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
+    }
+    return hostPath
+
 }
+
 module.exports = {
-    normalizeURL  
-}
+    normalizeURL  }
